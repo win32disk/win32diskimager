5484b3e27d5aa9308cac69fa71fac9a2f9f59511
Hiroshi Koyama <siriussk8er@users.sourceforge.net>
2018-06-07 15:26:23 -0700

    Add Japanese translation

---
+++ src/lang/diskimager_ja.ts
@@ -0,0 +1,515 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="29"/>
+        <source>Win32 Disk Imager</source>
+        <translation>Win32 Disk Imager</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="54"/>
+        <source>Image File</source>
+        <translation>イメージファイル</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="78"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="92"/>
+        <source>Device</source>
+        <translation>デバイス</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="128"/>
+        <source>Hash</source>
+        <translation>ハッシュ値</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="148"/>
+        <source>Select hash type for verification</source>
+        <translation>照合の方式</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="152"/>
+        <source>None</source>
+        <translation>無し</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="166"/>
+        <source>Generate selected hash on file</source>
+        <translation>ファイルから作成</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>Generate</source>
+        <translation>作成</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <source>Copy hash to clipboard</source>
+        <translation>ハッシュをクリップボードからコピー</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="191"/>
+        <source>Copy</source>
+        <translation>コピー</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="246"/>
+        <source>Read Only Allocated Partitions</source>
+        <translation>定義されたパーテーションの最後までしか読み取らない</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="268"/>
+        <source>Progress</source>
+        <translation>進捗の状況</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="295"/>
+        <source>%p%</source>
+        <translation>%p%</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="316"/>
+        <source>Cancel current process.</source>
+        <translation>作業をキャンセルしました。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="319"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="335"/>
+        <source>Read data from &apos;Device&apos; to &apos;Image File&apos;</source>
+        <translation>デバイスからファイルを作成</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="338"/>
+        <source>Read</source>
+        <translation>読込み</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="351"/>
+        <source>Write data from &apos;Image File&apos; to &apos;Device&apos;</source>
+        <translation>ファイルからデバイスに書込み</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="354"/>
+        <source>Write</source>
+        <translation>書込み</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="361"/>
+        <source>Verify the image file with the selected drive</source>
+        <translation>イメージとドライブの照合</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="364"/>
+        <source>Verify Only</source>
+        <translation>照合のみ</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="377"/>
+        <source>Exit Win32 Disk Imager</source>
+        <translation>Win32 Disk Imagerを終了</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="380"/>
+        <source>Exit</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="175"/>
+        <location filename="../mainwindow.cpp" line="185"/>
+        <location filename="../mainwindow.cpp" line="195"/>
+        <source>Exit?</source>
+        <translation>終了しますか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="175"/>
+        <source>Exiting now will result in a corrupt image file.
+Are you sure you want to exit?</source>
+        <translation>終了すると、破損したイメージファイルが作成されます。
+本当に終了してもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="185"/>
+        <source>Exiting now will result in a corrupt disk.
+Are you sure you want to exit?</source>
+        <translation>終了すると、破損したディスクが作成されます。
+本当に終了してもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="219"/>
+        <source>Select a disk image</source>
+        <translation>イメージを選択</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="262"/>
+        <source>Generating...</source>
+        <translation>生成中...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="296"/>
+        <location filename="../mainwindow.cpp" line="305"/>
+        <source>Cancel?</source>
+        <translation>キャンセル？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="296"/>
+        <source>Canceling now will result in a corrupt destination.
+Are you sure you want to cancel?</source>
+        <translation>いまキャンセルすると、宛先が破損します。
+本当にキャンセルしてもよろしいです？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="326"/>
+        <location filename="../mainwindow.cpp" line="608"/>
+        <location filename="../mainwindow.cpp" line="709"/>
+        <source>Write Error</source>
+        <translation>書き込みエラー</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="326"/>
+        <location filename="../mainwindow.cpp" line="608"/>
+        <location filename="../mainwindow.cpp" line="823"/>
+        <source>Image file cannot be located on the target device.</source>
+        <translation>イメージファイルをデバイスに配置できません。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="336"/>
+        <source>Confirm overwrite</source>
+        <translation>上書きの確認</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="336"/>
+        <source>Writing to a physical device can corrupt the device.
+(Target Device: %1 &quot;%2&quot;)
+Are you sure you want to continue?</source>
+        <translation>物理デバイスに書き込むとデバイスが破損する可能性があります。
+（ターゲットデバイス：%1 &quot;%2&quot;）
+続行してもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <source>Waiting for a task.</source>
+        <translation>タスクを待っています。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="195"/>
+        <source>Exiting now will cancel verifying image.
+Are you sure you want to exit?</source>
+        <translation>終了すると、照合がキャンセルされます。
+本当に終了してもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="217"/>
+        <source>Disk Images (*.img *.IMG);;*.*</source>
+        <translation>ディスクイメージ (*.img *.IMG);;*.*</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="305"/>
+        <source>Cancel Verify.
+Are you sure you want to cancel?</source>
+        <translation>照合をキャンセルします。
+本当にキャンセルしてもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="475"/>
+        <source>Not enough available space!</source>
+        <translation>空き容量が足りません！</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="561"/>
+        <location filename="../mainwindow.cpp" line="566"/>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <location filename="../mainwindow.cpp" line="585"/>
+        <location filename="../mainwindow.cpp" line="1056"/>
+        <location filename="../mainwindow.cpp" line="1061"/>
+        <location filename="../mainwindow.cpp" line="1066"/>
+        <location filename="../mainwindow.cpp" line="1079"/>
+        <source>File Error</source>
+        <translation>ファイルエラー</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="561"/>
+        <location filename="../mainwindow.cpp" line="1056"/>
+        <source>The selected file does not exist.</source>
+        <translation>選択したファイルは存在しません。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="566"/>
+        <location filename="../mainwindow.cpp" line="1061"/>
+        <source>You do not have permision to read the selected file.</source>
+        <translation>選択したファイルを読み込むための権限がありません。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <location filename="../mainwindow.cpp" line="1066"/>
+        <source>The specified file contains no data.</source>
+        <translation>指定されたファイルにはデータが含まれていません。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="575"/>
+        <location filename="../mainwindow.cpp" line="788"/>
+        <location filename="../mainwindow.cpp" line="1070"/>
+        <source>Done.</source>
+        <translation>完了しました。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="579"/>
+        <location filename="../mainwindow.cpp" line="792"/>
+        <location filename="../mainwindow.cpp" line="794"/>
+        <location filename="../mainwindow.cpp" line="1074"/>
+        <source>Complete</source>
+        <translation>完成</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="579"/>
+        <source>Write Successful.</source>
+        <translation>書き込み成功。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="585"/>
+        <location filename="../mainwindow.cpp" line="1079"/>
+        <source>Please specify an image file to use.</source>
+        <translation>使用するイメージファイルを指定してください。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="614"/>
+        <source>Confirm Overwrite</source>
+        <translation>上書きの確認</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="614"/>
+        <source>Are you sure you want to overwrite the specified file?</source>
+        <translation>ファイルを上書きしてもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="709"/>
+        <source>Disk is not large enough for the specified image.</source>
+        <translation>指定されたイメージに対し ディスク容量が十分ではありません。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="792"/>
+        <source>Read Canceled.</source>
+        <translation>読み込みがキャンセルされました。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="794"/>
+        <source>Read Successful.</source>
+        <translation>読み込み成功。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="801"/>
+        <source>File Info</source>
+        <translation>ファイル情報</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="801"/>
+        <source>Please specify a file to save data to.</source>
+        <translation>データを保存するファイルを指定してください。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="823"/>
+        <source>Verify Error</source>
+        <translation>照合エラー</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="957"/>
+        <source>Size Mismatch!</source>
+        <translation>サイズが合いません！</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1002"/>
+        <location filename="../mainwindow.cpp" line="1018"/>
+        <source>Verify Failure</source>
+        <translation>照合失敗</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1002"/>
+        <location filename="../mainwindow.cpp" line="1018"/>
+        <source>Verification failed at sector: %1</source>
+        <translation>セクター：%1で照合に失敗しました</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1074"/>
+        <source>Verify Successful.</source>
+        <translation>照合に成功しました。</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../disk.cpp" line="43"/>
+        <location filename="../disk.cpp" line="249"/>
+        <location filename="../disk.cpp" line="339"/>
+        <location filename="../disk.cpp" line="354"/>
+        <source>File Error</source>
+        <translation>ファイルエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="43"/>
+        <source>An error occurred when attempting to get a handle on the file.
+Error %1: %2</source>
+        <translation>ファイルのハンドルを取得しようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="77"/>
+        <location filename="../disk.cpp" line="224"/>
+        <source>Device Error</source>
+        <translation>デバイスエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="78"/>
+        <source>An error occurred when attempting to get a handle on the device.
+Error %1: %2</source>
+        <translation>デバイス上でハンドルを取得しようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="59"/>
+        <location filename="../disk.cpp" line="96"/>
+        <location filename="../disk.cpp" line="445"/>
+        <source>Volume Error</source>
+        <translation>ボリュームエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="60"/>
+        <source>An error occurred when attempting to get information on volume.
+Error %1: %2</source>
+        <translation>ボリュームに関する情報を取得中にエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="97"/>
+        <source>An error occurred when attempting to get a handle on the volume.
+Error %1: %2</source>
+        <translation>ボリューム上でハンドルを取得しようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="114"/>
+        <source>Lock Error</source>
+        <translation>ロックエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="115"/>
+        <source>An error occurred when attempting to lock the volume.
+Error %1: %2</source>
+        <translation>ボリュームをロックしようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="132"/>
+        <source>Unlock Error</source>
+        <translation>アンロックエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="133"/>
+        <source>An error occurred when attempting to unlock the volume.
+Error %1: %2</source>
+        <translation>ボリュームのロックを解除しようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="150"/>
+        <source>Dismount Error</source>
+        <translation>マウント解除エラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="151"/>
+        <source>An error occurred when attempting to dismount the volume.
+Error %1: %2</source>
+        <translation>ボリュームをマウント解除しようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="178"/>
+        <source>Read Error</source>
+        <translation>読込みエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="179"/>
+        <source>An error occurred when attempting to read data from handle.
+Error %1: %2</source>
+        <translation>ハンドルからデータを読み取ろうとしているときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="205"/>
+        <source>Write Error</source>
+        <translation>書込みエラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="206"/>
+        <source>An error occurred when attempting to write data to handle.
+Error %1: %2</source>
+        <translation>データの書き込み中にエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="225"/>
+        <source>An error occurred when attempting to get the device&apos;s geometry.
+Error %1: %2</source>
+        <translation>デバイスのジオメトリを取得中にエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="250"/>
+        <source>An error occurred while getting the file size.
+Error %1: %2</source>
+        <translation>ファイルサイズを取得中にエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="273"/>
+        <source>Free Space Error</source>
+        <translation>空き領域エラー</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="274"/>
+        <source>Failed to get the free space on drive %1.
+Error %2: %3
+Checking of free space will be skipped.</source>
+        <translation>ドライブ%1の空きスペースを取得できませんでした。
+エラー%2：%3
+空き容量の確認はスキップされます。</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="340"/>
+        <source>An error occurred while getting the device number.
+This usually means something is currently accessing the device;please close all applications and try again.
+
+Error %1: %2</source>
+        <translation>デバイス番号の取得中にエラーが発生しました。
+これは、通常、デバイスに現在アクセスしていることを意味します。すべてのアプリケーションを閉じて、もう一度やり直してください。
+
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="355"/>
+        <source>An error occurred while querying the properties.
+This usually means something is currently accessing the device; please close all applications and try again.
+
+Error %1: %2</source>
+        <translation>プロパティの確認中にエラーが発生しました。
+これは通常、何かが現在デバイスにアクセスしていることを意味します。 すべてのアプリケーションを閉じてもう一度やり直してください。
+
+エラー%1：%2</translation>
+    </message>
+    <message>
+        <location filename="../disk.cpp" line="446"/>
+        <source>An error occurred when attempting to get a handle on %3.
+Error %1: %2</source>
+        <translation>%3のハンドルを取得しようとしたときにエラーが発生しました。
+エラー%1：%2</translation>
+    </message>
+</context>
+</TS>
